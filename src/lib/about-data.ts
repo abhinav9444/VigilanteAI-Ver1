8566cab9dccfd009f233c883df68d03df60fb298
Abhinav Kumar Singh <22051564@kiit.ac.in>
2025-10-27 19:46:26 +0000

    Dr. Ranjita Kumar Dash profile pic link : https://media.licdn.com/dms/im

--- src/lib/about-data.ts
+++ src/lib/about-data.ts
@@ -19,7 +19,7 @@ export const teamMembers: TeamMember[] = [
     name: 'Abhinav Kumar Singh',
     rollNumber: '22051564',
     linkedin: 'https://www.linkedin.com/in/abhinavkrsingh03/',
-    imageUrl: 'https://picsum.photos/seed/dev1/200/200',
+    imageUrl: 'https://media.licdn.com/dms/image/v2/C4D03AQGJv3A3CiijhQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1659711667949?e=1762992000&v=beta&t=UuEsknmuGFIzsAPS59tRxz-iUyK_xSbQJ9ha_H-Krt8',
   },
   {
     name: 'Aniket Kumar',
@@ -40,5 +40,5 @@ export const projectGuide: ProjectGuide = {
   designation: 'Professor, Dept. of Computer Science',
   website: 'https://cse.kiit.ac.in/profiles/ranjita-kumari-dash/',
   linkedin: 'https://in.linkedin.com/in/dr-ranjita-kumari-dash-27b41117',
-  imageUrl: 'https://picsum.photos/seed/guide/200/200',
+  imageUrl: 'https://media.licdn.com/dms/image/v2/C5103AQGyZUjlEkY86w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1542281173297?e=2147483647&v=beta&t=cHPtAX1oYAG3ojs1tBjY-4_NfcxLiU3e5ML0YzLgmqE',
 };
