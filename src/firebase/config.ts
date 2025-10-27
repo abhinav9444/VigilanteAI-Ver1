96812823f0b84026309c2bf48ad5c7e2e6425567
Abhinav Kumar Singh <22051564@kiit.ac.in>
2025-10-27 14:21:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9597909228-18b8b",
+  "appId": "1:750649949869:web:426a598738683005bbd024",
+  "apiKey": "AIzaSyBScGRzQYTY6WvhKAC-aJcSnyztoNx21kQ",
+  "authDomain": "studio-9597909228-18b8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "750649949869"
+};
